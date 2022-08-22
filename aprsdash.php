b73748b2ddc38ff3b74b8436e5eec89f7b77b304
Alfredo Vania - IZ7BOJ <38103662+IZ7BOJ@users.noreply.github.com>
2022-08-22 09:26:10 +0200

    updated aprs-map.info url

--- aprsdash.php
+++ aprsdash.php
@@ -93,7 +93,7 @@ $i=0;
 	</TABLE>
 <br><br>
 
-<iframe src="https://www.aprs-map.info/snamelist/<?php echo $stationsquery ?>/time/60"width="800" height="400">Alternative text for browsers that do not understand IFrames.</iframe>
+<iframe src="https://aprs-map.info/?snamelist=<?php echo $stationsquery ?>"width="800" height="400">Alternative text for browsers that do not understand IFrames.</iframe>
 
 <hr>
 APRS Vitality Dashboard V0.1 Beta by IZ7BOJ Alfredo<br>
